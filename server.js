12479577b6b022e28a2bf083c4367c8f9fc3ccec
MasonM <Mason.P.Marinko@gmail.com>
2020-07-09 17:06:52 -0600

    adding HTML

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true}));
 // parse incoming JSON data
@@ -83,6 +84,10 @@ app.post('/api/animals', (req, res) => {
         res.json(animal);   
     }
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
